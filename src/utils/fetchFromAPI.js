c7ab2ba45fc35b217b1fac8ef4ef259725a0a911
Umang-Sharma1 <hemangsharma1000@gmail.com>
2024-05-25 13:27:00 +0530

    Initial commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "7695f77410mshf0239679a1f8001p17a6fajsn1c835f4b629b",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
